69c6beab8b276b387444743983236f1602e01b3b
Allan <amaxwellblair@gmail.com>
2015-12-10 17:04:20 -0700

    initial commit of the sorting suite

---
+++ lib/bubble_sort.rb
@@ -0,0 +1,75 @@
+require 'pry'
+
+class BubbleSort
+  attr_accessor :input_array
+
+  def sort(input_array)
+    @input_array = input_array
+    if data_valid?
+      auxiliary_methods
+    else
+      sorting_array = input_array
+      while not_sorted?(sorting_array)
+        sorting_array.each.with_index do |unit, i|
+          left, right = sorting_array[i], sorting_array[i+1]
+          if swap?(left, right)
+            sorting_array[i], sorting_array[i+1] = right, left
+          end
+        end
+      end
+      return sorting_array
+    end
+  end
+
+  def swap?(left, right)
+    if !left.nil? && !right.nil?
+      left > right
+    else
+      false
+    end
+  end
+
+  def not_sorted?(array = input_array)
+    array.each.with_index do |unit, i|
+      if !array[i+1].nil?
+         return true if unit > array[i+1]
+      end
+    end
+    return false
+  end
+
+  def data_valid?(array = input_array)
+    if nothing?(array)
+      true
+    elsif single_element?(array)
+      true
+    elsif just_nil?
+      true
+    else
+      false
+    end
+  end
+
+  def auxiliary_methods(array = input_array)
+    if nothing?(array)
+      []
+    elsif single_element?(array)
+      array
+    elsif just_nil?
+      nil
+    end
+  end
+
+  def nothing?(array = input_array)
+    array.length == 0
+  end
+
+  def single_element?(array = input_array)
+    array.length == 1 && array.first != nil
+  end
+
+  def just_nil?(array = input_array)
+    array.any?{|unit| unit == nil}
+  end
+
+end
---
+++ test/insertion_sort_test.rb
@@ -0,0 +1,59 @@
+$LOAD_PATH.unshift("../lib/")
+
+require 'insertion_sort'
+require 'minitest'
+
+class InsertionSortTest < Minitest::Test
+attr_accessor :sorter
+
+  def setup
+    @sorter = InsertionSort.new
+  end
+
+  def test_sort_class
+    assert_equal InsertionSort, sorter.class
+  end
+
+  def test_sort_empty
+    assert_equal [], sorter.sort([])
+  end
+
+  def test_sort_nil
+    assert_equal nil, sorter.sort([nil])
+  end
+
+  def test_sort_one
+    assert_equal [3], sorter.sort([3])
+  end
+
+  def test_sort_two
+    skip
+    assert_equal [2,3], sorter.sort([3,2])
+  end
+
+  def test_sort_three
+    skip
+    assert_equal [1,3,10], sorter.sort([3,10,1])
+  end
+
+  def test_sort_alot
+    skip
+    assert_equal [1,2,3,4,5,6,7,8,9,10], sorter.sort([10,8,1,2,3,5,7,6,9,4])
+  end
+
+  def test_sort_alpha_three
+    skip
+    assert_equal %w(a b c d e), sorter.sort(%w(b a c e d))
+  end
+
+  def test_not_sort_true
+    skip
+    assert sorter.not_sorted?([10,8,1,2,3,5,7,6,9,4])
+  end
+
+  def test_not_sort_false
+    skip
+    refute sorter.not_sorted?([1,2,3,4,5,6])
+  end
+
+end
